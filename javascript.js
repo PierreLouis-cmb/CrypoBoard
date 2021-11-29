2308a5b2c6c926ac9370a6c8e3e590652c5dc007
PierreLouis-cmb <89901631+PierreLouis-cmb@users.noreply.github.com>
2021-11-29 21:29:14 +0100

    v1

--- javascript.js
+++ javascript.js
@@ -2,52 +2,176 @@ $(document).ready(function () {
 	//Déclaration des variable
 	let page = 0;
 	let perPage = 0;
+	let val_recherche = "";
 
 	//Variable pour récuperer des éléments HTML
 	$next = $('#next');
 	$back = $('#back');
 	$selectPage = $('#perPage');
+	$recherche = $('#search');
+	$renit = $('#renit');
 	$back.hide();
+	$input_serarch = $("#valsearch").val()
 	perPage = $selectPage.val();
-//console.log(perPage)
 
-//Sélécteur nombre de résultat par page
-	$selectPage.on('click', function(){
+
+
+	//Sélécteur nombre de résultat par page
+	$selectPage.on('click', function () {
 		perPage = $selectPage.val();
-		getCoins(page, perPage)
-		})
+		getCoins(page, perPage, val_recherche)
+	})
 	$selectPage.val();
 	console.log($selectPage.val());
 
 
-//Page suivante
+
+	//Page suivante
 	$next.on('click', function () {
 		console.log("Page suivante");
 		page += 10;
-		getCoins(page, perPage);
+		getCoins(page, perPage, val_recherche);
 		$back.show();
 	});
 
-//Revenir sur la page d'avant
+	//Revenir sur la page d'avant
 	$back.on('click', function () {
 		console.log("Page précendente");
 		page -= 10;
-		getCoins(page, perPage);
+		getCoins(page, perPage, val_recherche);
 		if (page === 0) {
 			$back.hide();
 		}
 	});
 
-//Appel de l'api
+	//Recherche Crypto
+	$recherche.on('click', function () {
+		let val_recherche = $("#valsearch").val()
+		getCoin(val_recherche);
+	});
+
+	//Rénitialiser le filtre
+	$renit.on('click', function () {
+		getCoins(page, perPage)
+		$("#valsearch").val("")
+	})
+
+
+	//Activation de l'autocompletion recherche crypto (jqueryUI)
+	$("#valsearch").focus(function () {
+		getAllCoins()
+	});
+
+//Appels des api
 	getCoins(page, perPage);
+	getFiats();
 });
 
-function getCoins(page,perPage) {
-	// Construction de l'URL, en utilisant le nombre de page si défini
+function getCoins(page, perPage) {
+	// Construction de l'URL, en utilisant le nombre d'élement par page à afficher et la pagination
 	let url = "https://api.coinstats.app/public/v1/coins";
-	url += "?skip=" + page + "&limit="+ perPage + "&currency=EUR";
+	url += "?skip=" + page + "&limit=" + perPage + "&currency=EUR";
 	console.log(url);
+	// Définition de l'appel Ajax
+	$.ajax({
+		//L'URL de la requête
+		url: url,
+		//La méthode d'envoi
+		method: "GET",
+		//Le format de réponse attendu
+		dataType: "json",
+	})
+		//Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
+		.done(function (response) {
+			let coins = response.coins;
+			//Initialisation du tableau
+			let tabCoins = "";
+			for (let i = 0; i < coins.length; i++) {
 
+				//Var pour voir si .priceChange1h est négatif ou positif, retourne -1 si negatif, 1 si positif et 0 si =0
+				let price1h = Math.sign(coins[i].priceChange1h);
+				//let price = Math.round(coins[i].price * 200) / 100
+				//console.log(price);
+
+				//Variable de l'api
+				let id = coins[i].id;
+				let price = coins[i].price.toFixed(4);
+				let rank = coins[i].rank;
+				let name = coins[i].name;
+				let icon = coins[i].icon;
+				//let price = coins[i].price;
+				let volume = coins[i].volume;
+				let priceChange1h = coins[i].priceChange1h;
+				let priceChange1d = coins[i].priceChange1d;
+				let priceChange1w = coins[i].priceChange1w;
+				let twitterUrl = coins[i].twitterUrl;
+				let exp = coins[i].exp;
+				let websiteUrl = coins[i].websiteUrl;
+				//Changement de couleur (Si négatif,positif ou neutre)
+				//init couleur
+
+
+				let back = "";
+				if (price1h === -1) {
+					back = "red";
+				} else if (price1h === 1) {
+					back = "green"
+				} else {
+					back = "black"
+				}
+				// $("#search").on("keyup", function () {
+				// 	var regval = new RegExp('^' + $(this).val(), "i");
+				//
+				// 	$("table tr:gt(1)").hide()
+				// 	$("table tr:gt(1)").filter(function () {
+				// 		var contenu = $(this).find('td:eq(1)').html();
+				// 		return contenu.match(regval);
+				// 	}).show();
+				// })
+				//test
+
+				//Construction tableau
+				tabCoins += '<tr>';
+				tabCoins += `<td> ${rank} </td>`;
+				tabCoins += `<td> <a href ="${websiteUrl}" title="Site officiel"><img src="${icon}" class="logo">${name}</a></td>`;
+				tabCoins += `<td> ${price} </td>`;
+				tabCoins += `<td> ${volume} </td>`;
+				//Poser la variable back dans la classe pour changer la couleur
+				tabCoins += `<td id="price1h_${id}" class="pos neg ${" " + back}"> ${priceChange1h} %  </td>`;
+				tabCoins += `<td id="price1d_${id}" class="pos neg ${" " + back}"> ${priceChange1d} %  </td>`;
+				tabCoins += `<td id="price1w_${id}" class="pos neg ${" " + back}"> ${priceChange1w} %  </td>`;
+				tabCoins += `<td>  <a href="${twitterUrl}" class="fa fa-twitter"></a> </td>`;
+
+				//Affichage des liens exp
+				if (exp) {
+					for (let i = 0; i < exp.length; i++) {
+						tabCoins += `<td>  <a href="${exp[i]}">lien ${i}</a></td>`;
+					}
+					// cryptoCoins += `</div> <button type="button" id="btn_${id}"> VOIR</button></td>`
+				}
+				// $('#btn'+ id).on('click', function(){
+				// 	alert("tes bo")
+				// 	$('.exp'+ id ).show();
+				// })
+				tabCoins += '</tr>';
+				document.getElementById("data").innerHTML = tabCoins;
+
+			}
+
+			// Affichage des boutons de changement de page
+			// afficheCacheBoutons(response);
+
+		})
+		//Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
+		.fail(function (error) {
+			alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+		})
+}
+
+function getCoin(val_recherche) {
+	// Construction de l'URL, en utilisant le nombre de page si défini
+	let url = "https://api.coinstats.app/public/v1/coins/";
+	url += val_recherche;
 	// Définition de l'appel Ajax
 	$.ajax({
 		//L'URL de la requête
@@ -60,76 +184,65 @@ function getCoins(page,perPage) {
 		//Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
 		.done(function (response) {
 
-			let coins = response.coins;
-			let cryptoCoins = "";
-			for (let i = 0; i < coins.length; i++) {
-
+			let coin = response.coin;
+			let tabCoin = "";
+			console.log(coin)
+			try {
 				//Var pour voir si .priceChange1h est négatif ou positif, retourne -1 si negatif
-				let price1h = Math.sign(coins[i].priceChange1h);
+				let price1h = Math.sign(coin.priceChange1h);
+				let price = Math.round(coin.price * 200) / 100
+				console.log(price);
 
 				//Variable de l'api
-				let id = coins[i].id;
-				let rank = coins[i].rank;
-				let name = coins[i].name;
-				let icon = coins[i].icon;
-				let price = coins[i].price;
-				let volume = coins[i].volume;
-				let priceChange1h = coins[i].priceChange1h;
-				let priceChange1d = coins[i].priceChange1d;
-				let priceChange1w = coins[i].priceChange1w;
-				let twitterUrl = coins[i].twitterUrl;
-				let exp = coins[i].exp;
-
+				let id = coin.id;
+				let rank = coin.rank;
+				let name = coin.name;
+				let icon = coin.icon;
+				//let price = coins[i].price;
+				let volume = coin.volume;
+				let priceChange1h = coin.priceChange1h;
+				let priceChange1d = coin.priceChange1d;
+				let priceChange1w = coin.priceChange1w;
+				let twitterUrl = coin.twitterUrl;
+				let exp = coin.exp;
 				//Changement de couleur
 				let back = "";
-				if (price1h === -1 ){
+				if (price1h === -1) {
 					back = "red";
-				}else if (price1h === 1){
+				} else if (price1h === 1) {
 					back = "green"
-				}else{
+				} else {
 					back = "black"
 				}
+				//test
+				//console.log($.inArray('Bitcoin',coins[i].name));
 				//Construction tableau
-				cryptoCoins += '<tr>';
-				cryptoCoins += `<td> ${rank} </td>`;
-				cryptoCoins += `<td> <img src="${icon}"> </img> ${name} </td>`;
-				cryptoCoins += `<td> ${price} </td>`;
-				cryptoCoins += `<td> ${volume} </td>`;
+				tabCoin += '<tr>';
+				tabCoin += `<td> ${rank} </td>`;
+				tabCoin += `<td> <img src="${icon}" class="logo">${name} </td>`;
+				tabCoin += `<td> ${price} </td>`;
+				tabCoin += `<td> ${volume} </td>`;
 				//Poser la variable back dans la classe pour changer la couleur
-				cryptoCoins += `<td id="price1h_${id}" class="pos neg ${" "+back}"> ${priceChange1h} %  </td>`;
-				cryptoCoins += `<td id="price1d_${id}" class="pos neg ${" "+back}"> ${priceChange1d} %  </td>`;
-				cryptoCoins += `<td id="price1w_${id}" class="pos neg ${" "+back}"> ${priceChange1w} %  </td>`;
-				cryptoCoins += `<td> <a href="${twitterUrl}" title="${twitterUrl}"> Twitter </a></td>`;
-
-				if(exp){
-					for (let i = 0; i < exp.length; i++){
-						cryptoCoins += `<td> <a href="${exp[i]}">Link ${i}</a> </td>`;
+				tabCoin += `<td id="price1h_${id}" class="pos neg ${" " + back}"> ${priceChange1h} %  </td>`;
+				tabCoin += `<td id="price1d_${id}" class="pos neg ${" " + back}"> ${priceChange1d} %  </td>`;
+				tabCoin += `<td id="price1w_${id}" class="pos neg ${" " + back}"> ${priceChange1w} %  </td>`;
+				tabCoin += `<td>  <a href="${twitterUrl}" class="fa fa-twitter"></a> </td>`;
+				//Peut etre le mettre dans une popup ?
+				if (exp) {
+					for (let i = 0; i < exp.length; i++) {
+						tabCoin += `<td>  <a href="${exp}">lien ${i}</a> </td>`;
+						//console.log(exp[i])
 					}
 				}
-				cryptoCoins += '</tr>';
-				document.getElementById("data").innerHTML = cryptoCoins;
 
-				// let myTd = document.querySelector("#price1h_" + id);
-				// myTd.style.backgroundColor = "#d93600";
-				// $test = $("#price1h_" + id);
-				//myTd.style.backgroundColor = "#d93600";
-				// if (price1h === -1 ){
-				// 	console.log(myTd);
-				// 	myTd.style.backgroundColor = "#d93600";
-				// 	//let test = $test.css('color', 'red')
-				// 	$("#price1h_" + id).addClass('left');
-				//
-				// }
-				// else {
-				// 	console.log(myTd);
-				// }
 
+				tabCoin += '</tr>';
+				document.getElementById("data").innerHTML = tabCoin;
+			} catch (e) {
+
+				alert("Aucun résultat(s) trouvé(s) pour cette saisie")
+				//getCoins()
 			}
-
-
-
-			// Affichage des boutons de changement de page
-			// afficheCacheBoutons(response);
 
 		})
 		//Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
@@ -139,7 +252,6 @@ function getCoins(page,perPage) {
 }
 
 
-//Tableau des monnaies fiat
 function getFiats() {
 	// Construction de l'URL, en utilisant le nombre de page si défini
 	let url = "https://api.coinstats.app/public/v1/fiats";
@@ -155,7 +267,7 @@ function getFiats() {
 		//Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
 		.done(function (response) {
 			let fiats = response;
-			let cryptofiats = "";
+			let tabfiats = "";
 			for (let i = 0; i < fiats.length; i++) {
 				//Var pour voir si .priceChange1h est négatif ou positif, retourne -1 si negatif
 				//let price1h = Math.sign(coins[i].priceChange1h);
@@ -167,13 +279,13 @@ function getFiats() {
 				let imageUrl = fiats[i].imageUrl;
 
 				//Construction tableau
-				cryptofiats += '<tr>';
-				cryptofiats += `<td> <img src="${imageUrl}"></td>`;
-				cryptofiats += `<td> ${name} </td>`;
-				cryptofiats += `<td> ${rate} </td>`;
-				cryptofiats += `<td> ${symbol} </td>`;
-				cryptofiats += '</tr>';
-				document.getElementById("data_fiats").innerHTML = cryptofiats;
+				tabfiats += '<tr>';
+				tabfiats += `<td> <img src="${imageUrl}" class="logo"></td>`;
+				tabfiats += `<td> ${name} </td>`;
+				tabfiats += `<td> ${rate} </td>`;
+				tabfiats += `<td> ${symbol} </td>`;
+				tabfiats += '</tr>';
+				document.getElementById("data_fiats").innerHTML = tabfiats;
 			}
 		})
 		//Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
@@ -181,5 +293,44 @@ function getFiats() {
 			alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
 		})
 }
-getFiats();
+
+
+function getAllCoins() {
+	// Construction de l'URL, en utilisant le nombre de page si défini
+	let url = "https://api.coinstats.app/public/v1/coins";
+	// Définition de l'appel Ajax
+	$.ajax({
+		//L'URL de la requête
+		url: url,
+		//La méthode d'envoi
+		method: "GET",
+		//Le format de réponse attendu
+		dataType: "json",
+	})
+		//Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
+		.done(function (response) {
+			let allCoins = response.coins;
+			let autocompleteArray = new Array();
+			for (let i = 0; i < allCoins.length; i++) {
+
+				//Variable de l'api
+				let id = allCoins[i].id;
+
+				//Construction array pour autocompletion
+				autocompleteArray[i] = id;
+
+			}
+			//Injection de l'array dans la liste du input
+			$("#valsearch").autocomplete({
+
+				source: autocompleteArray
+			});
+
+			console.log(autocompleteArray)
+		})
+		//Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
+		.fail(function (error) {
+			alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+		})
+}
 
